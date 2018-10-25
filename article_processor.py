4bd2fbe0787b1bdfc84dd5c2768296b735c9cc48
g0nta <mongolian.uxooi@gmail.com>
2018-10-25 15:49:35 +0000

    hogehoge

--- article_processor.py
+++ article_processor.py
@@ -1,9 +1,21 @@
 import sys
 import MeCab
 import pandas as pd
+import neologd_normalizer as norm
 
 if __name__ == "__main__":
 	m = MeCab.Tagger("-d /usr/lib/mecab/dic/mecab-ipadic-neologd")
 	df = pd.read_csv('./data/qiita/2018-04-01.csv')
-	line = df.head(1)
-	text = line['body']
+	body = df.iat[0,2]
+	normalized_body = norm.normalize_neologd(body)
+	lines = m.parse(normalized_body).splitlines()
+	words = []
+	
+	for line in lines:
+		chunks = line.split('\t')
+		print(line)
+		break
+		#if len(chunks) > 3: #and (chunks[3].startswith('動詞') or chunks[3].startswith('形容詞') or (chunks[3].startswith('名詞') and not chunks[3].startswith('名詞-数'))):
+		#	words.append(chunks[0])
+		#	print(chunks[0])
+
