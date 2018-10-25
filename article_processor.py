bcad5280b4285cd7564368df3263815477285c46
g0nta <mongolian.uxooi@gmail.com>
2018-10-25 15:07:26 +0000

    add script

--- article_processor.py
+++ article_processor.py
@@ -4,9 +4,6 @@ import pandas as pd
 
 if __name__ == "__main__":
 	m = MeCab.Tagger("-d /usr/lib/mecab/dic/mecab-ipadic-neologd")
-	df = pd.read_csv('./data/qiita/2018-10-17.csv')
+	df = pd.read_csv('./data/qiita/2018-04-01.csv')
 	line = df.head(1)
-	for column_name, item in line.iteritems():
-		print(column_name)
-		print(item[0])
-
+	text = line['body']
---
+++ script.py
@@ -0,0 +1,3 @@
+import crawler as c
+
+c.crawl_qiita('2018-04-01', '2018-10-01')
