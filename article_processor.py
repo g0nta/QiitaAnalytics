e286baea22e230b5e237f9d90803515e040dc7a0
g0nta <mongolian.uxooi@gmail.com>
2018-11-03 09:26:06 +0000

    modify obsolute paramter name

--- article_processor.py
+++ article_processor.py
@@ -8,6 +8,7 @@ from gensim.models.doc2vec import Doc2Vec
 from gensim.models.doc2vec import TaggedDocument
 
 if __name__ == "__main__":
+	csv.field_size_limit(5000000)
 	m = MeCab.Tagger(r"-Ochasen -d /usr/lib/mecab/dic/mecab-ipadic-neologd")
 	data_path = './data/qiita'
 	file_list = [f for f in os.listdir(data_path) if os.path.isfile(os.path.join(data_path,f))]
@@ -15,12 +16,17 @@ if __name__ == "__main__":
 	for file_path in file_list:
 		file_path = os.path.join(data_path, file_path)
 		with open(file_path) as f:
-			reader = csv.reader(f, delimiter=",")
+			reader = csv.reader((line.replace('\0','') for line in f), delimiter=",")
 			next(reader)
 			for article in reader:
 				body = article[2]
 				normalized_body = norm.normalize_neologd(body)
-				lines = m.parse(normalized_body).splitlines()
+				try:
+					lines = m.parse(normalized_body).splitlines()
+				except AttributeError as ae:
+					print('args:{0}'.format(ae.args))
+					print('article id:{0}'.format(article[0]))
+					continue
 				words = []
 				for line in lines:
 					chunks = line.split('\t')
@@ -30,6 +36,6 @@ if __name__ == "__main__":
 				trainings.append(TaggedDocument(words = words, tags=[article[0]]))
 				print(file_path)
 				print(len(words))
-	model = Doc2Vec(documents=trainings, size=150, alpha=0.0015, sample=1e-4, min_count=10, workers=4, iter=30)
+	model = Doc2Vec(documents=trainings, vector_size=150, alpha=0.0015, sample=1e-4, min_count=10, workers=4, epochs=30)
 	model.save('Doc2VecModel_150dim.model')
 
